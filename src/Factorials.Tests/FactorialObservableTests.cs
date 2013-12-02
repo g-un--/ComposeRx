f63916c731d125ac67a04406ba57975855de9bde
g-un-- <office.gino@gmail.com>
2013-12-02 13:22:00 +0200

    Created FactorialObservable

---
+++ src/Factorials.Tests/FactorialObservableTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using Xunit;
+
+namespace Factorials.Tests
+{
+    public class FactorialObservableTests
+    {
+        private readonly FactorialObservable _factorial;
+
+        public FactorialObservableTests()
+        {
+            _factorial = new FactorialObservable();
+        }
+
+        [Fact]
+        public void FactorialOf5Returns120()
+        {
+            ulong computedResult = 0;
+
+            _factorial.Get(Scheduler.Immediate)(5).Subscribe(result =>
+            {
+                computedResult = result;
+            });
+
+            Assert.Equal<ulong>(computedResult, 120);
+        }
+
+        [Fact]
+        public void FactorialOf1Returns1()
+        {
+            ulong computedResult = 0;
+
+            _factorial.Get(Scheduler.Immediate)(1).Subscribe(result =>
+            {
+                computedResult = result;
+            });
+
+            Assert.Equal<ulong>(computedResult, 1);
+        }
+    }
+}
---
+++ src/Factorials/FactorialObservable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace Factorials
+{
+    public class FactorialObservable
+    {
+        private class State
+        {
+            public ushort Value { get; set; }
+            public ulong Accumulator { get; set; }
+        }
+
+        public Func<ushort, IObservable<ulong>> Get(IScheduler scheduler)
+        {
+            return value => Observable.Create<ulong>(observer =>
+            {
+                var state = new State {Value = value, Accumulator = 1};
+
+                return scheduler.Schedule(state, (currentState, self) =>
+                {
+                    if (currentState.Value == 0)
+                    {
+                        observer.OnNext(currentState.Accumulator);
+                        observer.OnCompleted();
+                    }
+                    else
+                    {
+                        currentState.Accumulator *= currentState.Value;
+                        currentState.Value -= 1;
+                        self(currentState);
+                    }
+                });
+            });
+        }
+    }
+}
