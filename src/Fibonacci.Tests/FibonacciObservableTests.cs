1f88e9594c2555033e0aab2be281a40ccca30c1e
g-un-- <office.gino@gmail.com>
2013-12-09 12:51:08 +0200

    Created FibonacciObservable with state passed in scheduler to mimic tail recursion call

---
+++ src/Fibonacci.Tests/FibonacciObservableTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using Xunit;
+
+namespace Fibonacci.Tests
+{
+    public class FibonacciObservableTests
+    {
+        private readonly FibonacciObservable _fibonacci;
+
+        public FibonacciObservableTests()
+        {
+            _fibonacci = new FibonacciObservable();
+        }
+
+        [Fact]
+        public void FibonnaciItemNumber5Returns5()
+        {
+            ulong computedResult = 0;
+
+            _fibonacci.Get(Scheduler.Immediate)(5).Subscribe(result =>
+            {
+                computedResult = result;
+            });
+
+            Assert.Equal<ulong>(computedResult, 5);
+        }
+
+        [Fact]
+        public void FibonnaciItemNumber7Returns13()
+        {
+            ulong computedResult = 0;
+
+            _fibonacci.Get(Scheduler.Immediate)(7).Subscribe(result =>
+            {
+                computedResult = result;
+            });
+
+            Assert.Equal<ulong>(computedResult, 13);
+        }
+    }
+}
---
+++ src/Fibonacci/FibonacciObservable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace Fibonacci
+{
+    public class FibonacciObservable
+    {
+        private class State
+        {
+            public ulong Value { get; set; }
+            public ulong Accumulator { get; set; }
+            public uint CurrentItemNumber { get; set; }
+        }
+
+        public Func<ushort, IObservable<ulong>> Get(IScheduler scheduler)
+        {
+            return nthItem => Observable.Create<ulong>(observer =>
+            {
+                var state = new State { Value = 0, Accumulator = 1, CurrentItemNumber = 1 };
+
+                return scheduler.Schedule(state, (currentState, self) =>
+                {
+                    if (currentState.CurrentItemNumber == nthItem)
+                    {
+                        observer.OnNext(currentState.Accumulator);
+                        observer.OnCompleted();
+                    }
+                    else
+                    {
+                        currentState.Accumulator = currentState.Accumulator + currentState.Value;
+                        currentState.Value = currentState.Accumulator - currentState.Value;
+                        currentState.CurrentItemNumber += 1;
+                        self(currentState);
+                    }
+                });
+            });
+        }
+    }
+}
