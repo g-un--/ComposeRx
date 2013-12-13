d6712b3c8d8024d5fa77f585ea85ef39c08b068d
g-un-- <office.gino@gmail.com>
2013-12-13 10:52:05 +0200

    Created continuations as observable

--- src/ContinuationMonad.Tests/ContinuationExtensionsTests.cs
+++ src/ContinuationMonad.Tests/ContinuationExtensionsTests.cs
@@ -1,4 +1,7 @@
 ﻿using System.Globalization;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ContinuationMonad.Tests
@@ -6,7 +9,7 @@ namespace ContinuationMonad.Tests
     public class ContinuationExtensionsTests
     {
         [Fact]
-        public void TestContinuation()
+        public void TestContinuationMonad()
         {
             var query = from x in 1.ToContinuation<int, string>()
                         from y in 1.ToContinuation<int, string>()
@@ -15,6 +18,18 @@ namespace ContinuationMonad.Tests
            var result = query(x => x.ToString(CultureInfo.InvariantCulture));
 
            Assert.Equal(result, "2");
+        }
+
+        [Fact]
+        public async Task TestObservableContinuation()
+        {
+            var query = from x in 1.ToObservable(Scheduler.Immediate)
+                        from y in 1.ToObservable(Scheduler.Immediate)
+                        select x + y;
+
+            var result = await query.Select(x => x.ToString(CultureInfo.InvariantCulture)).Take(1);
+
+            Assert.Equal(result, "2");
         }
     }
 }
---
+++ src/ContinuationMonad/ObservableContinuation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace ContinuationMonad
+{
+    public static class ObservableContinuation
+    {
+        public static IObservable<T> ToObservable<T>(this T value, IScheduler scheduler)
+        {
+            return Observable.Return(value, scheduler);
+        }
+    }
+}
